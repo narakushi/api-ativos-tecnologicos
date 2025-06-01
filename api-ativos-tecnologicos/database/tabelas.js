4918420799bac7adc9dc8d582ff62c84a033feae
narakushi <taina.santana1305@gmail.com>
2025-05-31 22:19:57 -0300

    :sparkles: feat: aicionando conexão com banco de dados e criando 2 tabelas

---
+++ api-ativos-tecnologicos/database/tabelas.js
@@ -0,0 +1,58 @@
+class Tabelas {
+  init(conexao) {
+    this.conexao = conexao;
+    this.criarTipoAtores();
+    this.criarAtores();
+  }
+
+  //primeiro criar tabelas auxiliares
+  criarTipoAtores() {
+    const sql = `
+      CREATE TABLE IF NOT EXISTS tipo_atores(
+      ID_Tipo_Ator INTEGER AUTO_INCREMENT NOT NULL, 
+      Nome_Tipo_Ator ENUM("Empresa", "Startup", "ICT (UNEB, IF BAIANO, FATEC), Governo Municipal, Governo Estadual, Governo Federal", 
+      "Ambiente de inovação","Entidade de Apoio (Sebrae, SENAI,Associações), Investidor",
+      "Mentor"),
+      PRIMARY KEY (ID_Tipo_Ator)
+    ); `;
+
+    this.conexao.query(sql, (error) => {
+      if (error) {
+        console.log("deu erro", error);
+        return;
+      }
+      console.log("Tabela tipo_atores criada!");
+    })
+  }
+
+  criarAtores() {
+    const sql = `
+    CREATE TABLE IF NOT EXISTS atores_ecossistema(
+	  ID_Ator INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Nome_Oficial VARCHAR(255),
+    Nome_Fantasia VARCHAR(255),
+    CNPJ_CPF VARCHAR(255) UNIQUE,
+    ID_Tipo_Ator INTEGER, 
+	  FOREIGN KEY (ID_Tipo_Ator) REFERENCES tipo_atores(ID_Tipo_Ator)
+    );
+    `
+    try {
+      this.conexao.query(sql)
+      console.log("Tabela atores_ecossistema criada");
+    } catch (error) {
+      console.log("Ocorreu um erro na criação da tabela atores_ecossistema.");
+    }
+
+    //definindo chave estrangeira de atores_ecossistema
+    try {
+      this.conexao.query(this.conexao.query("ALTER TABLE atores_ecossistema ADD CONSTRAINT fk_ID_Tipo_Ator FOREIGN KEY (ID_Tipo_Ator) REFERENCES tipo_atores(ID_Tipo_Ator);"));
+      console.log("Chave estrangeira definida para atores_ecossistema!");
+    }
+    catch (error) {
+      console.log("Erro ao definir a chave estrangeira para atores_ecossistema");
+    }
+
+  }
+}
+
+module.exports = new Tabelas();
--- api-ativos-tecnologicos/index.js
+++ api-ativos-tecnologicos/index.js
@@ -2,7 +2,14 @@ const express = require("express");
 const app = express();
 const port = 3000
 const router = require("./routers/index");
+const tabelas = require("./database/tabelas");
+const conexao = require("./database/conexao.db");
 
+
+//acionando conxão com banco + criação das tabelas
+tabelas.init(conexao);
+
+//acionando rotas
 router(app);
 
 app.listen(port, (error) => {
